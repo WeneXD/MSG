dc827b15920d6e54ca9a0e603b34a5b72dbfdf01
WeneXD <72886782+WeneXD@users.noreply.github.com>
2022-11-10 09:02:41 +0200

    UI Update
    Able to "join" a room (create an user in the room)
    UI shows errors if unable to join room.

--- UI.py
+++ UI.py
@@ -11,11 +11,16 @@ token=""
 class mainFrame(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.title("MSG | Roomlist")
+        self.wm_attributes("-toolwindow",'True')
         self.geometry("512x384")
         self.minsize(512,384)
         self.maxsize(512,384)
 
-        self.title_font=font.Font(family="Helvetica",size="24",weight="bold")
+        self.title_font=font.Font(family="Javanese Text",size="24",weight="bold")
+        self.im_font=font.Font(family="Javanese Text",size="12",weight="bold")
+        self.alert_font=font.Font(family="KacstBook",size="8")
+        self.name_vcmd=(self.register(self.name_validate),'%P')
 
         container=tk.Frame(self,width=512,height=384)
         container.pack(side="top",fill="both",expand=True)
@@ -35,6 +40,13 @@ class mainFrame(tk.Tk):
     def show_frame(self,page_name):
         frame=self.frames[page_name]
         frame.tkraise()
+    
+    def name_validate(self,P):
+        if len(P)>=16:
+            return False
+        if len(P)<16:
+            return True
+    
 
 class StartPage(tk.Frame):
     def __init__(self,parent,controller):
@@ -42,23 +54,39 @@ class StartPage(tk.Frame):
         self.controller=controller
         Title=tk.Label(self,text="Room list",font=controller.title_font)
         Title.grid(column=0,row=0)
-        
+
+        #Name Label & Entrybox
+        name_label=tk.Label(self,text="Name",font=controller.im_font)
+        name_label.place(height=20,width=50,x=310,y=100)
+        self.name_entry=tk.Entry(self,validate="key",validatecommand=controller.name_vcmd)
+        self.name_entry.place(x=310,y=115)
+        #Room Password Label & Entrybox
+        rmPW_label=tk.Label(self,text="Room Password",font=controller.im_font)
+        rmPW_label.place(height=20,width=125,x=310,y=150)
+        self.rmPW_entry=tk.Entry(self)
+        self.rmPW_entry.place(x=310,y=165)
+        #Join Button
+        join_button=tk.Button(self,text="Join",command=self.join_room)
+        join_button.place(x=310,y=190)
+        #Alert Label
+        self.alert_label=tk.Label(self,text="",font=controller.alert_font)
+        self.alert_label.place(x=0,y=360)
+        #Roomlist
         self.RoomList=tk.Listbox(self)
-        
+        self.refresh_list()
+        #Setting up Roomlist
         self.RoomList.config(width=50)
         self.RoomList.grid(column=0,row=1,sticky="W",pady=5)
 
-        #button1=tk.Button(self,text="Join Room",command=lambda:controller.show_frame("Room"))
         #button2=tk.Button(self,text="Credits",command=lambda:controller.show_frame("Credits"))
         bt_clearRoomList=tk.Button(self,text="Refresh",command=self.refresh_list)
 
-        #button1.grid(column=0,row=1,sticky="W")
         #button2.grid(column=0,row=2,sticky="W")
-        bt_clearRoomList.grid(column=0,row=2)
+        bt_clearRoomList.grid(column=0,row=2,pady=10)
 
     def refresh_list(self):
         self.RoomList.delete(0,tk.END) #Clear roomlist
-        
+
         resp=req.get(addr+"/get_rooms/")
         meow=resp.json()
         if 'err' in meow:
@@ -68,7 +96,28 @@ class StartPage(tk.Frame):
                 if not rm[2]:
                     self.RoomList.insert(int(id),f"{rm[0]} | {rm[1]}")
                 else:
-                    self.RoomList.insert(int(id),f"{rm[0]} | {rm[1]} (Req pass)")
+                    self.RoomList.insert(int(id),f"{rm[0]} | {rm[1]} | req pass")
+
+    def join_room(self):
+        name=self.name_entry.get()
+        if len(name)==0:
+            self.alert_label.config(text="Error: Insert name")
+            return
+        rmPW=self.rmPW_entry.get()
+        rmSelect=self.RoomList.get(tk.ACTIVE)
+        if rmSelect=="(No Rooms, try creating one)":
+            return
+        room=rmSelect.split(" | ")
+        if len(room)==3:
+            resp=req.post(addr+f"/join_room/?roomID={room[0]}&name={name}&pw={rmPW}")
+        else:
+            resp=req.post(addr+f"/join_room/?roomID={room[0]}&name={name}")
+        meow=resp.json()
+        print(meow)
+        if 'err' in meow:
+            self.alert_label.config(text=f"Error: {meow['err']}")
+        else:
+            self.alert_label.config(text="Successfully joined")
 
 class Room(tk.Frame):
     def __init__(self,parent,controller):
